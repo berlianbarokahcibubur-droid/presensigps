8e89e8daca513195584a8e1082266b27d42a890e
BerlianBarokah <berlianbarokahcibubur@gmail.com>
2026-06-02 21:25:02 +0700

    Update logo BBB

--- resources/views/presensi/cetaklaporan.blade.php
+++ resources/views/presensi/cetaklaporan.blade.php
@@ -71,7 +71,7 @@
     <table style="width: 100%">
         <tr>
             <td style="width: 30px">
-                <img src="{{ asset('assets/img/logo-satu8.JPG') }}" width="70" height="70" alt="">
+                <img src="{{ asset('assets/img/logo-bbb.JPG') }}" width="70" height="70" alt="">
             </td>
             <td>
                 <span id="title">
